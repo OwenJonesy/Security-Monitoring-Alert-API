c842ef8217150657f04d520c947bb6fc96e6a6a6
OwenJonesy <C21377551@mytudublin.ie>
2025-09-19 21:52:03 +0100

    Initial commit: Spring Boot project setup with Docker configs

---
+++ src/main/java/com/jones/security_alert_api/SecurityAlertApiApplication.java
@@ -0,0 +1,13 @@
+package com.jones.security_alert_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAlertApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityAlertApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jones/security_alert_api/SecurityAlertApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jones.security_alert_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityAlertApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
